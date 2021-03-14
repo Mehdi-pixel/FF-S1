ce93f8e2f2399275b60aa4ec1d2dce95ea826844
mehdi-pixel <mehdi050998@gmail.com>
2021-03-14 16:09:52 +0100

    Clignotement 100% implémenté
    Le clignotement fonctionne à 100% maintenant, avec les bonnes valeurs.
    
    Il ne reste plus que la gestion du rapport cyclique avec Intensité et l'interruption par UART.

--- FF-S1.c
+++ FF-S1.c
@@ -14,12 +14,13 @@
 #include "FF-S1_Divers.h"
 
 unsigned char Intensite=0xA; //On met à 10% comme valeur d'exemple
-unsigned char Lum_ON=2;
-unsigned char Lum_OFF=250;
+unsigned char Lum_ON=20;
+unsigned char Lum_OFF=60;
 unsigned char Lum_Nbre=10;
 int currentNum = 0;
-int one_mil = 0x100; // Nombre arbitraire, on changera plus tard à 1 ds
-int cpt = 0;
+int one_cs = 20828; //Nombre de cycles processeur pour attendre 1cs.
+int cptON = 0;
+int cptOFF = 0;
 int divTimer = 0;
 
 sbit FREQ_OUT = P3^2;
@@ -34,27 +35,31 @@ int i=1;
 
 
 void Lumiere(unsigned char Intensite,unsigned char Lum_ON,unsigned char Lum_OFF,unsigned char Lum_Nbre){
-		if (currentNum != Lum_Nbre && currentNum%2 == 0 && Lum_ON != 0 && cpt != one_mil*Lum_ON){
-			//On garde allumé (à l'intensité qu'on veut) pendant Lum_ON millisecondes
-			cpt = cpt+1;
+		if (currentNum != Lum_Nbre && Lum_ON != 0 && cptON != 20*Lum_ON){
+			//On garde allumé (à l'intensité qu'on veut) pendant Lum_ON décisecondes
+			cptON++;
 			if(FREQ_OUT) {
-				TMR3RL = 0xE018; //Normalement variable avec Intensite
+				TMR3RL = 0xFFFF - 16662; //Normalement variable avec Intensite
 		}
 			else {
-				TMR3RL = 0xF7DF; // On ne garde PAS à 65535, ça cause des erreurs
+				TMR3RL = 0xFFFF - 4166; // On ne garde PAS à 65535, ça cause des erreurs
 		}
-			//Générateur de signaux
-			FREQ_OUT = !FREQ_OUT;
-			Reset_Timer3Overflow;
-			return ;
+		//Générateur de signaux
+		FREQ_OUT = !FREQ_OUT;
+		Reset_Timer3Overflow;
+		cptOFF = 0; //Reset du compteur de Lum_OFF pour son prochain allumage
+		return ;
 	}
-		else if (currentNum != Lum_Nbre && Lum_OFF !=0){
+		else if (currentNum != Lum_Nbre && Lum_OFF != 0 && cptOFF != 20*Lum_OFF){
 			//On garde éteint pendant Lum_OFF millisecondes => On met FREQ_OUT à 0 et on ne rentre plus dans l'ISR pendant Lum_OFF secondes
+			cptOFF++;
 			FREQ_OUT = 0;
-			TMR3RL = (0xFFFF-(one_mil*Lum_OFF));
-			currentNum = currentNum + 1;			//Le processeur NE VEUT PAS lire cette ligne, sans raison particulière
+			TMR3RL = 0xFFFF - 10414; //On le met à la même période que le ON ((TMRRL dutyON + TMRRL dutyOFF )/ 2)
 			Reset_Timer3Overflow;
-			cpt = 0; //Reset du compteur de Lum_ON pour son prochain allumage
+			if ( cptOFF == 20*Lum_OFF) {
+				currentNum++;
+				cptON = 0; //Reset du compteur de Lum_ON pour son prochain allumage
+			}
 			return ; //On sort immédiatement de la fonction
 		}
 		//Gestion du rapport cyclique : Mode basique sans clignotement
@@ -113,12 +118,5 @@ void main (void) {
 
 void ISR_timer3Overflow() interrupt 14
 {
-	if (divTimer == 2){
 		Lumiere(Intensite,Lum_ON, Lum_OFF, Lum_Nbre);
-		divTimer = 0;
-	}
-	else{
-		Reset_Timer3Overflow;
-	}
-	divTimer = divTimer + 1;
 }
